bd3fafeeacb8daa08e9b7f5cd3648480a234ceec
awLann <alwanfajrul20@gmail.com>
2023-05-29 20:21:15 +0700

    update register routing

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('index');
 });
 
+Route::get('/register', function () {
+    return view('register');
+});
+
 Route::get('/login', function () {
     return view('login');
 });
